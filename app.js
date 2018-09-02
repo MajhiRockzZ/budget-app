44f4c56220be6b7c3c4e0b0699f45f515b876a98
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-02 11:15:04 +0530

    Add listner for Enter key

--- app.js
+++ app.js
@@ -18,9 +18,29 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+    
+    
 
     document.querySelector('.add__btn').addEventListener('click', function() {
-       console.log("Button was clicked!") 
+        
+        // 1. Get the filed input data
+        
+        // 2. Add the item to the budget controller
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget on the UI
+        
+    });
+    
+    document.addEventListener('keypress', function(event) {
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            
+        }
+        
     });
     
 })(budgetController, UIController);

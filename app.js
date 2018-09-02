cc9671beeb760e3563998ec60bb999e59c66375c
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-02 12:39:47 +0530

    Remove Experiment Data

--- app.js
+++ app.js
@@ -17,17 +17,6 @@ var budgetController = (function () {
 })();
 
 
-/////////////////////////////////////
-// Experiment Data
-////////////////////////////////////
-var Expense = function (id, description, value) {
-    this.id = id;
-    this.description = description;
-    this.value = value;
-};
-/* -------------------------------------- */
-
-
 // UI CONTROLLER
 var UIController = (function () {
 

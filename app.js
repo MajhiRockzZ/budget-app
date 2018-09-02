bb953403a224c859f09e3cd84c7064d7a8bc2d76
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-02 12:35:53 +0530

    Add experiment data

--- app.js
+++ app.js
@@ -20,10 +20,12 @@ var budgetController = (function () {
 /////////////////////////////////////
 // Experiment Data
 ////////////////////////////////////
-
-
-
---------------------------------------
+var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+/* -------------------------------------- */
 
 
 // UI CONTROLLER

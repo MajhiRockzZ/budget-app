2bd4e94e718255103202acb88eaaee1518cbc89f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-02 11:18:42 +0530

    Add function for add button

--- app.js
+++ app.js
@@ -18,29 +18,30 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-    
-    
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    var ctrlAddItem = function () {
         
         // 1. Get the filed input data
-        
+
         // 2. Add the item to the budget controller
-        
+
         // 3. Add the item to the UI
-        
+
         // 4. Calculate the budget
-        
+
         // 5. Display the budget on the UI
         
-    });
-    
-    document.addEventListener('keypress', function(event) {
-        
+        console.log('Its works');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+
         if (event.keyCode === 13 || event.which === 13) {
-            
+            ctrlAddItem();
         }
-        
+
     });
-    
+
 })(budgetController, UIController);

91ecf156ac08d6f42c20a3dc40074e600ed6aed8
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-05 10:17:03 +0530

    Un-fixible code error

--- app.js
+++ app.js
@@ -22,23 +22,62 @@ var budgetController = (function () {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+
+
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //[1 2 3 4 5], next ID = 6
+            //[1 2 4 6 8], next ID = 9
+            // ID = last ID + 1
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
+        }
+    };
 
 })();
 
 
-// UI CONTROLLER
+
+
+// UI CONTROLLER (OK)
 var UIController = (function () {
 
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
     };
 
     return {
-        getinput: function () {
+        getInput: function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
@@ -55,35 +94,49 @@ var UIController = (function () {
 
 
 
+
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function () {
+        var input, newItem;
 
-        // 1. Get the filed input data
-        var input = UICtrl.getinput();
-        console.log(input);
+        // 1. Get the field input data
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
-        // 4. Calculate the budget
+        // 4. Clear the fields
 
-        // 5. Display the budget on the UI
+        // 5. Calculate and update budget
 
+
+        // 6. Calculate and update percentages
+};
+
+return {
+    init: function () {
+        console.log('Application has started.');
+        setupEventListeners();
     }
-
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-
-    });
+};
 
 })(budgetController, UIController);
+
+
+controller.init();

ba14705f9fea7fbe01e01543a769e92773386a79
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-06 16:10:37 +0530

    Replace placeholder text

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ var UIController = (function () {
         },
 
         addListItem: function (obj, type) {
-            var html;
+            var html, newHtml;
 
             // Create HTML string with placeholder text
 
@@ -96,13 +96,13 @@ var UIController = (function () {
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
-
-
-
-
             // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%values%', obj.value);
 
             // Insert the HTML into the DOM
+
 
         },
 

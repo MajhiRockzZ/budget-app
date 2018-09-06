7b6beddeef2a8f5cc599a562be338d6aa9c4a41d
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-06 19:38:59 +0530

    Bug fixed: length error

--- app.js
+++ app.js
@@ -24,9 +24,6 @@ var budgetController = (function () {
         }
     };
 
-
-
-
     return {
         addItem: function (type, des, val) {
             var newItem, ID;
@@ -74,6 +71,8 @@ var UIController = (function () {
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
@@ -86,13 +85,17 @@ var UIController = (function () {
         },
 
         addListItem: function (obj, type) {
-            var html, newHtml;
+            var html, newHtml, element;
 
             // Create HTML string with placeholder text
 
             if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
@@ -102,6 +105,7 @@ var UIController = (function () {
             newHtml = newHtml.replace('%values%', obj.value);
 
             // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
 
         },

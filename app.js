28d6e4f621ce1fcef3084b0eb8bd687e6e90f3c9
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-06 16:05:56 +0530

    Add replacement string

--- app.js
+++ app.js
@@ -85,14 +85,20 @@ var UIController = (function () {
             };
         },
 
-        addListItem: function(obj, type) {
+        addListItem: function (obj, type) {
             var html;
 
             // Create HTML string with placeholder text
 
-            html = '<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>' ;
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
 
-            html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+
+
+
 
             // Replace the placeholder text with some actual data
 
@@ -142,14 +148,14 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 
         // 6. Calculate and update percentages
-};
+    };
 
-return {
-    init: function () {
-        console.log('Application has started.');
-        setupEventListeners();
-    }
-};
+    return {
+        init: function () {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
 
 })(budgetController, UIController);
 

797c5c917d13a93d1f8cf9abfbb079810f08e2f6
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-06 15:51:33 +0530

    Add method addListItem

--- app.js
+++ app.js
@@ -85,6 +85,16 @@ var UIController = (function () {
             };
         },
 
+        addListItem: function(obj, type) {
+
+            // Create HTML string with placeholder text
+
+            // Replace the placeholder text with some actual data
+
+            // Insert the HTML into the DOM
+
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
